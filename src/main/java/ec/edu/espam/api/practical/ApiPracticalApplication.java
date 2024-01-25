62f33e35076856dc05e2c283011ab7a4a37f335b
Yelro <vanorley20@gmail.com>
2024-01-25 13:04:54 -0500

    Feature: init

---
+++ src/main/java/ec/edu/espam/api/practical/ApiPracticalApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espam.api.practical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPracticalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espam/api/practical/ApiPracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espam.api.practical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPracticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
